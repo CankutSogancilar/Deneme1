993e5b37dded1b8ae56194f084138aae6fa2de2d
cankut <cankutsogancilar@gmail.com>
2023-06-26 18:28:39 -0400

    Deneme added

---
+++ src/Deneme/Deneme1.java
@@ -0,0 +1,7 @@
+package Deneme;
+
+public class Deneme1 {
+    public void deneme1(){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/Deneme2/Deneme1.java
@@ -0,0 +1,7 @@
+package Deneme2;
+
+public class Deneme1 {
+    public void deneme(){
+        System.out.println("Hello");
+    }
+}
